0ca599776cc775a3906bbde26afa9ba951d98c2a
ajinkyapatilofficial <ajinkyapatilofficial07@gmail.com>
2022-12-28 18:47:58 +0530

    created char,owelscount&LoopAssignment.js
    created char,owelscount&LoopAssignment.js

---
+++ 05_countCharAssign.js
@@ -0,0 +1,16 @@
+function countCharA(sentence){
+    counter=0;
+    for(let index=0;index<=sentence.length;index++)
+    {
+         var char=sentence.charAt(index);
+        if(char=='A' || char=='a')
+        {
+            counter=counter+1;
+        }
+    }
+    console.log(`${sentence}`);
+    console.log(`Total Number of 'A' or 'a' in the given String is :${counter}`);
+}
+countCharA(`"I Am Learning JavaScript ,The Language of Internet"`);
+console.log(`--------------------------------------------------------------------`);
+countCharA(`"My Favourite movie is LAggAn"`);
---
+++ 05_whileLoopAssign.js
@@ -0,0 +1,56 @@
+console.log(`==========(*) Using While Loop (*)========== \n`);
+//Print the numbers from 5 to 15 by incrementing 1
+//initilization 5 condition i<=15 update expre i++
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++
+}
+console.log(`----------------------------------------------------------------`);
+//Print the numbers from 50 to 40 by decrementing by 1
+//initilization 50 condition i>=40 update expre i--
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i--
+}
+console.log(`----------------------------------------------------------------`);
+//WAP to print first 15 odd numbers
+//initilization 1 condition i<=15 update expre i=i+2
+var i=1;
+while (i<=15) {
+    console.log(i);
+    i=i+2
+}
+console.log(`----------------------------------------------------------------`);
+//WAP to find first 10 even Numbers
+//initilization 2 condition i<=20 update expre i=i+2
+var i=2;
+while (i<=20) {
+    console.log(i);
+    i=i+2
+}
+console.log(`----------------------------------------------------------------`);
+//WAP to print table of 5 like --> 5,10,15,.........50
+//initilization 5 condition i<=50 update expre i=i+5
+var i=5;
+while (i<=50) {
+    console.log(i);
+    i=i+5
+}
+console.log(`----------------------------------------------------------------`);
+//WAP to print table of 10 like 10,20,30,......100
+//initilization 10 condition i<=100 update expre i=i+10
+var i=10;
+while (i<=100) {
+    console.log(i);
+    i=i+10
+}
+console.log(`----------------------------------------------------------------`);
+//WAP to print table of 10 in reverse order -->100,90,80---------10
+//initilization 100 condition i>=10 update expre i=i-10
+var i=100;
+while (i>=10) {
+    console.log(i);
+    i=i-10
+}
